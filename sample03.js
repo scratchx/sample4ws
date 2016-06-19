4576a138728f62aa44439b391ecadd4619090779
scratchx <nobu.kk@gmail.com>
2016-06-19 18:38:28 +0900

    Add files via upload

---
+++ sample03.js
@@ -0,0 +1,107 @@
+/*
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+(function(ext) {
+
+  ext.latestUserTweet = function(name, callback) {
+    $.ajax({
+      method: "GET",
+      url: "http://scratchx-twitter.herokuapp.com/1.1/statuses/user_timeline.json",
+      data: {
+        screen_name: name,
+        count: 1
+      },
+      dataType: "json",
+      success: function(data) {
+        if (data.length > 0) {
+          callback(data[0].text);
+          return;
+        }
+        callback("No tweets found");
+      },
+      error: function(xhr, textStatus, error) {
+        console.log(error);
+        callback();
+      }
+    });
+  };
+
+  ext.getTopTweet = function(sort, str, callback) {
+    //If searching popluar, remove # and @ symbols from query
+    if (sort == "popular") {
+      str = str.replace('#','').replace('@','');
+    }
+    $.ajax({
+      method: "GET",
+      url: "http://scratchx-twitter.herokuapp.com/1.1/search/tweets.json",
+      data: {
+        q: encodeURIComponent(str),
+        result_type: sort,
+        count: 1
+      },
+      dataType: "json",
+      success: function(data) {
+        if (data.statuses.length > 0) {
+          callback(data.statuses[0].text);
+          return;
+        }
+        callback("No tweets found");
+      },
+      error: function(xhr, textStatus, error) {
+        console.log(error);
+        callback();
+      }
+    });
+  };
+
+
+    ext.speak_text = function (text, callback) {
+        var u = new SpeechSynthesisUtterance(text.toString());
+        u.onend = function(event) {
+            if (typeof callback=="function") callback();
+        };
+        
+        speechSynthesis.speak(u);
+    };
+
+    ext._shutdown = function() {};
+
+
+   ext._getStatus = function() {
+        if (window.SpeechSynthesisUtterance === undefined) {
+            return {status: 1, msg: 'Your browser does not support text to speech. Try using Google Chrome or Safari.'};
+        }
+        return {status: 2, msg: 'Ready'};
+    };
+
+
+  var descriptor = {
+    blocks: [
+      ['R', 'latest tweet from @%s', 'latestUserTweet', 'scratch'],
+      ['R', 'most %m.sort tweet containing %s', 'getTopTweet', 'recent', '#scratch'],
+      ['w', 'speak %s', 'speak_text', 'Hello!'], 
+
+    ],
+    menus: {
+      sort: ["popular", "recent"]
+    },
+    url: 'https://dev.twitter.com/overview/documentation'
+  };
+
+  ScratchExtensions.register('Twitter and Text2Speech', descriptor, ext);
+
+})({});
+
+
+
