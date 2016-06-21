1e36501d86b74f4451a37082a6b653ac11150a9c
scratchx <nobu.kk@gmail.com>
2016-06-21 22:18:41 +0900

    Update test01.js

--- test01.js
+++ test01.js
@@ -1,1 +1,10 @@
+// extension packs for ws
+
+// twitter
 ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
+// text to speech
+ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js);
+// speech recognition
+ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
+
+
