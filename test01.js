e5933d425ed8e8931ae383b1c5a084c88bedd085
scratchx <nobu.kk@gmail.com>
2016-06-21 22:41:53 +0900

    Update test01.js

--- test01.js
+++ test01.js
@@ -5,6 +5,8 @@ ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extensio
 // text to speech
 ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js");
 // speech recognition
-ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
+ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/speech_to_text_extension.js");
+// kinect v1 suport
+ScratchExtensions.loadExternalJS("http://stephenhowell.github.io/Kinect2ScratchX.js");
 
 
