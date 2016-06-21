e23742b7014bda71a8072c8af76b332030cb2de4
scratchx <nobu.kk@gmail.com>
2016-06-21 22:19:06 +0900

    Update test01.js

--- test01.js
+++ test01.js
@@ -3,7 +3,7 @@
 // twitter
 ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
 // text to speech
-ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js);
+ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js");
 // speech recognition
 ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
 
