267c1fd60f007226214f3c8e7a58b5aa8032b949
scratchx <nobu.kk@gmail.com>
2016-06-22 22:55:01 +0900

    Update test01.js

--- test01.js
+++ test01.js
@@ -6,11 +6,13 @@ ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-di
 // Link Opner:
 ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions/link-opener.js");
 // Browser Info and Language:
-//ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/Browser_Info_and_Language.js");
+ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/Browser_Info_and_Language.js");
 // text to speech: needs windows
 ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js");
 // speech recognition: needs windows
 ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/speech_to_text_extension.js");
 // kinect v1 suport: needs windows and kinect v1
 ScratchExtensions.loadExternalJS("http://stephenhowell.github.io/Kinect2ScratchX.js");
+// extension Loader
+ScratchExtensions.loadExternalJS("http://thepuzzlegame.github.io/extensions/Extension_Loader.js");
 // end
