56dd9241853fe34add121973e5a4c8a7a4d4cad2
scratchx <nobu.kk@gmail.com>
2016-06-22 20:11:02 +0900

    Update test01.js

--- test01.js
+++ test01.js
@@ -1,12 +1,16 @@
 // extension packs for ws
-
 // twitter
 ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
-// text to speech
+// RSS reader:
+ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/FeedMe_RSS_fun.js");
+// Link Opner:
+ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions/link-opener.js");
+// Browser Info and Language:
+ScratchExtensions.loadExternalJS("xtensions-directory/extensions/Browser_Info_and_Language.js);
+// text to speech: needs windows
 ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js");
-// speech recognition
+// speech recognition: needs windows
 ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/speech_to_text_extension.js");
-// kinect v1 suport
+// kinect v1 suport: needs windows and kinect v1
 ScratchExtensions.loadExternalJS("http://stephenhowell.github.io/Kinect2ScratchX.js");
-
-
+// end
